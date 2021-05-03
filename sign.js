631bb65d7c1794ec49570b7ee28a01b5fba0fc23
TAVAU <yuhanwang001@gmail.com>
2021-05-02 23:39:12 -0400

    Digital Signatures

--- sign.js
+++ sign.js
@@ -4,17 +4,19 @@ const SHA256 = require('crypto-js/sha256');
 const ec = new EC('secp256k1');
 
 // TODO: fill in your hex private key
-const privateKey = "";
+const privateKey = "9f39ca3bebb38e5779808561704fce4054632ee90ee455295f15f6fa01ef6d68";
 
 const key = ec.keyFromPrivate(privateKey);
 
 // TODO: change this message to whatever you would like to sign
-const message = "I am in the ChainShot Bootcamp";
+const message = "I am Yuhan";
 
 const msgHash = SHA256(message);
-
+//console.log(typeof msgHash)// obj
+console.log(msgHash)
 const signature = key.sign(msgHash.toString());
-
+console.log(signature)
+console.log(typeof signature)
 console.log({
   message,
   signature: {
--- verify.js
+++ verify.js
@@ -5,8 +5,8 @@ const ec = new EC('secp256k1');
 
 // TODO: fill in the public key points
 const publicKey = {
-  x: "",
-  y: ""
+  x: "891e768e4886594d3e8ec41d91f972599d8d37a7814cd23c5be9e1b2ad1e3a0f",
+  y: "16e1ea3979a8faecf0b0a76b32422e75776fcd1ca43e275b1e1403d480bb7751"
 }
 
 const key = ec.keyFromPublic(publicKey, 'hex');
@@ -17,8 +17,8 @@ const msgHash = SHA256(msg).toString();
 
 // TODO: fill in the signature components
 const signature = {
-  r: "",
-  s: ""
+  r: "36683c8a19b2e69a152c8e81154221c35613bf7d05dce865c5f3112e4c3d47eb",
+  s: "5e1109366536b8886cfed7c8b4169fd138e331ae472528e9dff1913ed5187159"
 };
 
 console.log(key.verify(msgHash, signature));
